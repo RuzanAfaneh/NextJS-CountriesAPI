3d640f8bdc2234f0775917db6611d92eb8da07b5
RuzanAfaneh <ruzanafaneh@gmail.com>
2021-06-13 02:25:19 +0300

    add config to fix deply

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const aliasPathsToResolve = [
+    { name: 'components', path: path.resolve(__dirname, './components') },
+    { name: 'Common', path: path.resolve(__dirname, '../../common/react/') },
+]
+module.exports = () => {
+    return  {
+        webpack(config, { defaultLoaders }) {
+            config.module.rules.push({
+	        test: /\.(js|jsx)$/,
+	        include: [path.resolve(__dirname, '../../common/react/')],
+	        use: [defaultLoaders.babel],
+	    })
+
+            /** Resolve aliases */
+	    aliasPathsToResolve.forEach((module) => {
+	        config.resolve.alias[module.name] = module.path
+	    })
+        }
+    }
+}
