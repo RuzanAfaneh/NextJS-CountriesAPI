954942373cde1f6a53a1c293b6c746e12fc646e4
RuzanAfaneh <ruzanafaneh@gmail.com>
2021-06-20 02:37:29 +0300

    cash control ?

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export async function getStaticProps(context) {
 
   return {
     props: { data }, // will be passed to the page component as props
-    revalidate: 1,
+    revalidate: 20,
   };
 }
 
